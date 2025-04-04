b7432059c066e1000e67552a0f886a883eff0499
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2025-04-04 20:33:15 +0000

    chore: add a delcarative version

--- Is the string uppercase?/app.js
+++ Is the string uppercase?/app.js
@@ -15,10 +15,10 @@
 
 // Method: Compare each character against an uppercase version if it
 
-
+// Imperative version
 function isAllCaps(string) {
     const upperCasedString = string.toUpperCase()
-    
+
     for(let i = 0; i < string.length; i++) {
         if(string[i] != upperCasedString[i]) 
             return false
@@ -26,5 +26,21 @@ function isAllCaps(string) {
     return true
 }
 
-console.log(isAllCaps('ACSKLDFJSGSKLDFJSKLDFJ'))
-console.log(isAllCaps('hello I AM DONALD'))
+// console.log(isAllCaps('ACSKLDFJSGSKLDFJSKLDFJ'))
+// console.log(isAllCaps('hello I AM DONALD'))
+
+
+// Declarative version
+function isAllCapsV2(string) {
+    return [...string].every(char => char.toUpperCase() === char)
+}
+
+console.log(isAllCapsV2('ACSKLDFJSGSKLDFJSKLDFJ'))
+console.log(isAllCapsV2('hello I AM DONALD'))
+
+/**
+ * MDN Array.prototype.every()
+ * The every() method of Array instances tests whether all 
+ * elements in the array pass the test implemented by the provided function. 
+ * It returns a Boolean value.
+ */
