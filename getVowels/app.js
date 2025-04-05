3d90cd31bee046fdd094cb4935f6817261902e3b
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2025-04-05 11:37:50 +0200

    chore: new exercise

---
+++ getVowels/app.js
@@ -0,0 +1,21 @@
+const vowels = ['a', 'e', 'i', 'o', 'u']
+
+function getVowels(str) {
+  const arr = []
+  for(let i = 0; i < str.length; i++) {
+    if(vowels.includes(str[i].toLowerCase())) { // We gotta make sure we perform case-insensitivity here
+      arr.push(str[i])
+    }
+  }
+  return arr.length === 0 ? 0:arr
+}
+
+// console.log(getVowels('HELLo'))
+
+// with regex
+function getVowelsRegEx(str) {
+  const regEx = str.match(/[aeiou]/gi)
+  
+  return !regEx ? 0: regEx
+}
+console.log(getVowelsRegEx('Hellou'))
