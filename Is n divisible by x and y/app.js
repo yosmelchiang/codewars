896ccc78c0fe549df959fe2fa4dd27e236944364
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2025-04-05 17:13:05 +0000

    refactor: add a verbose version

--- Is n divisible by x and y/app.js
+++ Is n divisible by x and y/app.js
@@ -11,17 +11,30 @@ function isDivisible(n, x, y) {
     return Number.isInteger(n / x) && Number.isInteger(n / y)
 }
 
-console.log(isDivisible(3, 1, 3))
-console.log(isDivisible(12, 2, 6))
-console.log(isDivisible(100, 5, 3))
-console.log(isDivisible(12, 7, 5))
+// console.log(isDivisible(3, 1, 3))
+// console.log(isDivisible(12, 2, 6))
+// console.log(isDivisible(100, 5, 3))
+// console.log(isDivisible(12, 7, 5))
 
 // We are using the modulus here
 function isDivisibleV2(n, x, y) {
-    return n % x === 0 && n % y === 0
+    const divisibleByX = n % x === 0;
+    const divisibleByY = n % y === 0;
+    
+    return divisibleByX && divisibleByY;
 }
 
 console.log(isDivisibleV2(3, 1, 3))
 console.log(isDivisibleV2(12, 2, 6))
 console.log(isDivisibleV2(100, 5, 3))
 console.log(isDivisibleV2(12, 7, 5))
+
+// We are using the modulus here - Minimalistic
+function isDivisibleV3(n, x, y) {
+    return n % x === 0 && n % y === 0
+}
+
+// console.log(isDivisibleV3(3, 1, 3))
+// console.log(isDivisibleV3(12, 2, 6))
+// console.log(isDivisibleV3(100, 5, 3))
+// console.log(isDivisibleV3(12, 7, 5))
