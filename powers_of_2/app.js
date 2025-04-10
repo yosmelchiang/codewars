a20b319aec6381577d8e37312e42950ae222d9be
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2025-04-10 12:42:36 +0200

    chore: new exerise: powerOfTwo

---
+++ powers_of_2/app.js
@@ -0,0 +1,41 @@
+// Takes a non-integer n as input
+// Returns a list of all the powers of 2 ranging from 0 and up to n (n inclusive)
+
+// Examples:
+// n = 0 = [1] // [2^0]
+// n = 1 = [1, 2]
+// n = 2 = [1, 2, 4]
+
+// Iterations:
+// i = 0: 2^0
+function powerOfTwo(n) {
+  let arr = []; // Populate this array with the calculation results for each i
+
+  for(let i = 0; i <= n; i++) {
+    arr.push(2**i)
+  }
+  return arr;
+}
+
+// console.log(powerOfTwo(0)) // [1]
+// console.log(powerOfTwo(1)) // [1, 2]
+// console.log(powerOfTwo(2)) // [1, 2, 4]
+
+function powerOfTwoV2(n) {
+  let arr = [];
+  
+  for(let i = 0; i <= n; i++) {
+    arr.push(Math.pow(2, i))
+  }
+  return arr;
+}
+// console.log(powerOfTwoV2(0)) // [1]
+// console.log(powerOfTwoV2(1)) // [1, 2]
+// console.log(powerOfTwoV2(2)) // [1, 2, 4]
+
+function powerOfTwoV3(n) {
+  return [...Array(n + 1)].map((_, i) => 2**i)
+}
+console.log(powerOfTwoV3(0)) // [1]
+console.log(powerOfTwoV3(1)) // [1, 2]
+console.log(powerOfTwoV3(2)) // [1, 2, 4]
