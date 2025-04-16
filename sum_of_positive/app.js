12c8a6dfafe514402a504cb5370a880966d61bbd
yosmelchiang <yosmel.chiang@gmail.com>
2025-04-16 14:17:32 +0200

    add new exercise: sum_of_positive

---
+++ sum_of_positive/app.js
@@ -0,0 +1,11 @@
+function sumOfPositives(arr) {
+    let result = 0;
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] > 0){
+            result += arr[i]
+        }
+    }
+    return result;
+}
+
+console.log(sumOfPositives([1, -4, 7, 12]));
