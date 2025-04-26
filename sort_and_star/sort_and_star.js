472d22d3e505a58aef3cb121efe6f97f203eae28
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2025-04-26 08:26:30 +0200

    chore: add new exercise, sort_and_star

---
+++ sort_and_star/sort_and_star.js
@@ -0,0 +1,7 @@
+// const myArr = ["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"];
+// const myArr = ["turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones"];
+// console.log(myArr.sort().shift().split('').join('***'))
+
+function twoSort(s) {
+  return s.sort().shift().split('').join('***');
+}
