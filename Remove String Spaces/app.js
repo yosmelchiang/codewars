07c90c6a36a5294ce783519b9da0efd1c5b0701c
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2025-06-10 00:49:02 +0200

    chore: new exercise

---
+++ Remove String Spaces/app.js
@@ -0,0 +1,28 @@
+// Write a function that removes the spaces of a string and returns the string without spaces
+
+/**
+ * Data sample:
+ * "8 j 8   mBliB8g  imjB8B8  jl  B" -> "8j8mBliB8gimjB8B8jlB"
+ * "8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd" -> "88Bifk8hB8BB8BBBB888chl8BhBfd"
+ * "8aaaaa dddd r     " -> "8aaaaaddddr"
+ */
+
+// Imperative
+function removeSpaces(str) {
+  const arr = str.split(' '); // split the string so we individual elements in an array
+  let result = ''; // Variable where we are going to save characters that are not space
+
+  // Iterate through each element and save only those that are not space chars
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] !== ' ') result += arr[i]
+  }
+
+  return result;
+}
+
+// Declarative
+function removeSpacesModern(str) {
+  return str.split(' ').filter(char => char !== ' ').join('');
+}
+
+console.log(removeSpacesModern('8 j 8   mBliB8g  imjB8B8  jl  B'))
