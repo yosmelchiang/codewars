33f5a4234f58fcae013782914cbfa9807a582431
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2025-06-10 00:53:35 +0200

    refactor: no need to convert to an array, we can iterate a string just as it is

--- Remove String Spaces/app.js
+++ Remove String Spaces/app.js
@@ -9,20 +9,20 @@
 
 // Imperative
 function removeSpaces(str) {
-  const arr = str.split(' '); // split the string so we individual elements in an array
   let result = ''; // Variable where we are going to save characters that are not space
 
-  // Iterate through each element and save only those that are not space chars
-  for(let i = 0; i < arr.length; i++) {
-    if(arr[i] !== ' ') result += arr[i]
+  // Iterate through char save only those that are not space chars
+  for(let i = 0; i < str.length; i++) {
+    if(str[i] !== ' ') result += str[i]
   }
 
   return result;
 }
+console.log(removeSpaces('8 j 8   mBliB8g  imjB8B8  jl  B'))
 
 // Declarative
 function removeSpacesModern(str) {
   return str.split(' ').filter(char => char !== ' ').join('');
 }
 
-console.log(removeSpacesModern('8 j 8   mBliB8g  imjB8B8  jl  B'))
+// console.log(removeSpacesModern('8 j 8   mBliB8g  imjB8B8  jl  B'))
