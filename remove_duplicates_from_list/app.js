73851390f607a37aa68cb54495bfb501cbd9bb8b
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2025-06-10 19:27:33 +0200

    chore: new exercise

---
+++ remove_duplicates_from_list/app.js
@@ -0,0 +1,16 @@
+// We want a function that removes duplicates from an array
+  // The number have to be non negative
+  // The list without duplicates have to be removed
+
+function distinct(a) {
+  const arr = [];
+
+  for(let i = 0; i < a.length; i++) {
+    if(!arr.includes(a[i])) arr.push(a[i])
+  }
+
+  return arr;
+}
+
+console.log(distinct([1, 1, 2]));
+console.log(distinct([1, 2, 1, 1, 3, 2]));
