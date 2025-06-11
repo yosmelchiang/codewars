bf18abe5177d14560cad7a9f35c2419fdd8b87a9
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2025-06-11 22:05:58 +0200

    new exercise

---
+++ Are_you_playing_banjo/app.js
@@ -0,0 +1,7 @@
+function areYouPlayingBanjo(name) {
+  return name.toLowerCase().startsWith('r') ? `${name} play banjo` : `${name} does not play banjo`;
+};
+
+console.log(areYouPlayingBanjo('Rolf'));
+console.log(areYouPlayingBanjo('Martin'));
+
