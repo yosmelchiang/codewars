8e8aa9b814357993b74339b4f4daf5357db85672
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2025-06-12 22:00:42 +0200

    new exercise

---
+++ Kata_example_twist/app.js
@@ -0,0 +1,17 @@
+// add the value "codewars" to the websites array 1,000 times
+let websites = []
+
+// Using a for loop
+for(let i = 0; i < 1000; i++) {
+  websites.push('codewars');
+}
+
+// console.log(websites);
+
+// Using rest operator
+const newFillArr = [...Array(1000)].fill('codewars');
+// console.log(newFillArr);
+
+// Using keybword new
+const newNewArr = new Array(1000).fill('codewars');
+// console.log(newNewArr);
