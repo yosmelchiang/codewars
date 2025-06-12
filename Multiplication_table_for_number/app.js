6ee092dff8ea595a062e30b0b3ff4da0769fbc48
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2025-06-12 10:27:36 +0200

    new exercise

---
+++ Multiplication_table_for_number/app.js
@@ -0,0 +1,22 @@
+// Using strings
+function multiTable(number) {
+  let table = ''
+  for(let i = 1; i <= 10; i++) {
+    table += `${i} * ${number} = ${number * i}\n`
+  }
+  
+  return table.slice(0, -1);
+}
+
+console.log(multiTable(5))
+
+// Using an array
+function multiTableArray(number) {
+  let table = []
+  for(let i = 1; i <= 10; i++) {
+    table.push(`${i} * ${number} = ${number * i}`)
+  }
+  return table.join('\n')
+}
+
+console.log(multiTableArray(5))
