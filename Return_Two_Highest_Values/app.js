312ff353543a7f342c49dadaaf116ed640b435b6
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2025-06-12 22:23:55 +0200

    new exercise, imperative solution

---
+++ Return_Two_Highest_Values/app.js
@@ -0,0 +1,35 @@
+const log = console.log
+
+// Return the two unique highest values in a list
+// The result needs to be in descending order
+// Examples
+/**
+ * [], []
+ * [15], [15]
+ * [15, 20, 20, 17], [20, 17]
+ */
+
+// We must sort the array
+    // we do that using the .sort method and a sorting function for descending order
+// We need to only keep uniques
+    // We are creating a new array, checking if the current item is already in that array
+    // If its not, push it
+// We need to make sure we are only left with 2 numbers
+    // Only push items to the uniques array that do not already exist there, and for as long as the length of the array is less than 2
+    // If its highers than 2, dont push
+    
+function twoHighest(arr) {
+  const sortedArr = arr.sort((a, b) => b - a)
+  const uniques = [];
+
+  for(item of sortedArr) {
+    if(!uniques.includes(item) && uniques.length < 2) uniques.push(item);
+  }
+
+  return uniques;
+}
+
+log(twoHighest([9, 10, 10, 10, 8])) // [10, 9];
+log(twoHighest([15, 20, 20, 17])) // [20, 17];
+log(twoHighest([15])) // [15];
+log(twoHighest([])) // [];
