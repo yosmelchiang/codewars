af7fff8b3d77452dfc64d42469cdbfac9dcfb558
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2025-06-14 14:32:28 +0200

    chore: add logic comments

--- Trimming_a_string/app.js
+++ Trimming_a_string/app.js
@@ -8,6 +8,13 @@ function trim(str, size) {
     return str.slice(0, size-3) + '...'
 }
 
+// Logic
+/**
+ * If the string is short enough, meaning its less than size equal to size, then it doesnt need trimming, return it as is
+ * If the size provided is less than or equal to 3, trim the string down to that size, then add the dots later
+ * If the string is greater than 3 and greater than size, trim it it by size - 3 (allowing space for the 3 dots)
+ */
+
 console.log(trim('Creating kata is fun', 14))
 console.log(trim('He', 1))
 console.log(trim("Code Wars is pretty rad", 50))
