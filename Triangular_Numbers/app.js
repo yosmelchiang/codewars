8ff5ff84e29535b5f0bd09410944f999f2920c1d
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2025-06-15 20:02:11 +0200

    add a few more alternative solutions

--- Triangular_Numbers/app.js
+++ Triangular_Numbers/app.js
@@ -1,4 +1,5 @@
-function triangular( n ) {
+// Using loops
+function triangular(n) {
     // Outer loop > builds each row
     // Inner loop > builds the amount of dots inside each row
 
@@ -6,18 +7,18 @@ function triangular( n ) {
     const arr = [];
 
     // Row
-    for(let i = 0; i < n; i++) {
+    for (let i = 0; i < n; i++) {
         const dots = []
 
         // Dots
-        for(let j = 0; j <= i; j++) {
+        for (let j = 0; j <= i; j++) {
             dots.push('*')
         }
 
         arr.push(dots)
     }
 
-    return arr.join('').replaceAll(',','').length
+    return arr.join('').replaceAll(',', '').length
 };
 
 // Examples
@@ -25,12 +26,43 @@ function triangular( n ) {
 // console.log(triangular(4)) // 10
 // console.log(triangular(-454)) // 0
 
+// Using one loop
+function triangularLoop(n) {
+    let result = 0;
+
+    for(let i = 1; i <= n; i++) {
+        result += i
+    }
+
+    return result;
+}
+
+// console.log(triangularLoop(2)) // 3
+// console.log(triangularLoop(4)) // 10
+// console.log(triangularLoop(-454)) // 0
+
+// Using a while loop
+function triangularWhile(n) {
+    let result = 0;
+    
+    while (n > 0) {
+        result += n
+        n--
+    }
+    
+    return result;
+}
+
+// console.log(triangularWhile(2)) // 3
+// console.log(triangularWhile(4)) // 10
+// console.log(triangularWhile(-454)) // 0
+
 // Using Math
 // Triangular formula is n * ( n + 1 ) / 2
 function triangularMath(n) {
-    return n === 0 || n < 0 ? 0 : n * (n + 1) / 2
+    return n <= 0 ? 0 : n * (n + 1) / 2
 }
 
-console.log(triangularMath(2)) // 3
-console.log(triangularMath(4)) // 10
-console.log(triangularMath(-454)) // 0
+// console.log(triangularMath(2)) // 3
+// console.log(triangularMath(4)) // 10
+// console.log(triangularMath(-454)) // 0
